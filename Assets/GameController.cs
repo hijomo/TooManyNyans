1c4ba6ac99fd6c614d3aec402c02eab3684a1a31
hijomo <hijomo@gmail.com>
2017-12-02 00:11:07 -0700

    pack for submission

---
+++ Assets/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+  public GameObject nyan;
+  float nextNyan;
+
+	// Use this for initialization
+	void Start () {
+    nextNyan = Time.time + 2.5f;
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    if (nextNyan < Time.time)
+    {
+      Instantiate(nyan, Vector3.zero, Quaternion.identity);
+      nextNyan = Time.time + 2.5f;
+    }
+	}
+}
---
+++ Assets/NyanController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NyanController : MonoBehaviour {
+
+  public AudioClip nyanAudio1;
+  public AudioClip nyanAudio2;
+  public AudioClip nyanAudio3;
+
+  public float nyanSpeed = 200f;
+  AudioSource auS;
+  Rigidbody rib;
+  // Use this for initialization
+  void Start () {
+    rib = gameObject.GetComponent<Rigidbody>();
+    auS = gameObject.GetComponent<AudioSource>();
+    int i = Random.Range(1, 4);
+
+    switch (i)
+    {
+      case 1: 
+        auS.clip = nyanAudio1;
+       
+        break;
+      case 2:
+        auS.clip = nyanAudio2;
+       
+        break;
+      case 3:
+      
+        auS.clip = nyanAudio3;
+        break;
+
+      default:
+       
+        auS.clip = nyanAudio1;
+        
+        break;
+    }
+    auS.Play();
+    Vector3 force = Random.insideUnitSphere * nyanSpeed;
+    rib.AddForce(force);
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(rib.velocity.magnitude < 5f)
+    {
+      Vector3 force = Random.insideUnitSphere * nyanSpeed;
+      rib.AddForce(force);
+    }
+	}
+
+  private void OnMouseDown()
+  {
+    Destroy(gameObject);
+  }
+}
